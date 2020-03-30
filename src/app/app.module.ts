d36de198328299505df6a3f4481e65f35b0554fd
Nicholas Mendez <snickdx@gmail.com>
2020-03-30 21:29:13 +0000

    added boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 //Start angular material components
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -28,6 +29,7 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatDialogModule,
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule
   ],
