e9c9feec58d77275a2009aeba819c3176c956b46
Snickdx <snickdx@gmail.com>
2020-04-07 02:12:09 -0400

    updated mat modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //Start angular material components
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatListModule} from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 //End angular material components
 
 @NgModule({
@@ -21,13 +22,15 @@ import {MatDialogModule} from '@angular/material/dialog';
     AppComponent
   ],
   imports: [
+    MatCheckboxModule,
+    MatListModule,
+    MatSnackBarModule,
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
     MatInputModule,
-    MatTableModule,
-    MatDialogModule,
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
